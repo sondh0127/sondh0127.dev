9f302dc0b5ce1298ff8ffbebe77999fa1c6ddda5
Son Hong Do <sondh0127@gmail.com>
2024-03-13 23:30:57 +0700

    b

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'NuxtHub Everhour',
-  description: 'Customize everhour frontend',
+  title: 'Son Hong Do Portfolio',
+  description: 'sondh0127.dev',
 })
 </script>
 
